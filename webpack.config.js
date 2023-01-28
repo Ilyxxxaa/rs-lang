1d8a1a3075fa926929de6739dd5b019050e6a362
Ilyxxxa <ilyxakotsur@gmail.com>
2023-01-28 15:20:13 +0300

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+
+const baseConfig = {
+  entry: {
+    main: path.resolve(__dirname, './src/main.ts'),
+  },
+  mode: 'development',
+  devServer: {
+    hot: isDev,
+    liveReload: true,
+    port: 4200,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp|mp3)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets/images',
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/main.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets',
+          to: 'assets',
+        },
+      ],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode
+    ? require('./webpack.prod.config')
+    : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
